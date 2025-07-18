5e172b3d44e73a2f69f418e6a8bfb05aac79f294
Reza <r.mehralizadeh@gmail.com>
2025-07-18 12:46:00 +0330

    feat(condditions): added to name and email and phone

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,36 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import {useForm} from 'react-hook-form' 
+import * as yup from 'yup'
 import { useNavigate } from 'react-router-dom'
 const Register = () => {
     const navigate = useNavigate()
-    const {handleSubmit , register} = useForm() 
+    const checkPhone = (item) => {
+        if (!item) {
+            return false
+        }
+        if (item.slice(0,2) === '09' && item.length===11 && /^\d+$/.test(item.slice(2,11))) {
+            return true
+        } else if (item.slice(0,1) === '+' && item.length===13 && /^\d+$/.test(item.slice(1,13))) {
+            return true
+        } else {
+            return false
+        }
+    }
+    const schema = yup.object({
+        firstName: yup.string().required().min(3).max(255),
+        lastName: yup.string().required().min(3).max(255),
+        email: yup.string().required().email(),
+        phone: yup.string().required().test('check-phone', 'Write phone number in correct form',checkPhone)
+    })
+    const {handleSubmit , register, formState} = useForm({
+        defaultValues: {
+            firstName: 'name'
+        },
+        resolver: yupResolver(schema)
+    })
+    
     const Submitted = (data) => {
-        console.log('submit', data)
+        alert('submit', data)
     }
     const homeButton = () => {
         navigate('/')
@@ -15,30 +41,37 @@ const Register = () => {
                 <div>
                     <label>First Name :</label>
                     <input type="text" {...register('firstName')} />
+                    <div>{formState.errors.firstName && formState.errors.firstName.message}</div>
                 </div>
                 <div>
                     <label>Last Name :</label>
                     <input type="text" {...register('lastName')} />
+                    <div>{formState.errors.lastName && formState.errors.lastName.message}</div>
                 </div>
                 <div>
                     <label>Email :</label>
                     <input type="text" {...register('email')} />
+                    <div>{formState.errors.email && formState.errors.email.message}</div>
                 </div>
                 <div>
                     <label>Phone :</label>
                     <input type="text" {...register('phone')} />
+                    <div>{formState.errors.phone && formState.errors.phone.message}</div>
                 </div>
                 <div>
                     <label>Username :</label>
                     <input type="text" {...register('username')} />
+                    <div>{formState.errors.username && formState.errors.username.message}</div>
                 </div>
                 <div>
                     <label>Password :</label>
                     <input type="text" {...register('password')} />
+                    <div>{formState.errors.password && formState.errors.password.message}</div>
                 </div>
                 <div>
                     <label>Confirm Password :</label>
                     <input type="text" {...register('confirm')} />
+                    <div>{formState.errors.confirm && formState.errors.confirm.message}</div>
                 </div>
                 <button type='submit'>Register</button>
                 <button onClick={homeButton}>Back</button>
